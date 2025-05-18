79abfbc042b4f07a6ca3d263ef9b60f5816bf1df
SangamSilwal <sangamsilwal2062@gmail.com>
2025-05-18 18:02:39 +0545

    Handling file in Golang

--- 15Files/main.go
+++ 15Files/main.go
@@ -4,31 +4,48 @@ package main
 import (
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 )
 
+// func main() {
+// 	fmt.Println("Hey WelCome to Golang")
+// 	content := "This is the text that Goes into the file"
+// 	file, err := os.Create("./myFile.txt")
+// 	if err != nil {
+// 		panic(err)
+// 		//Panic will shout down the exection of the program
+// 	}
+// 	length, err := io.WriteString(file, content)
+// 	//io.WriteString will give err and length
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("Length is: ", length)
+// 	defer file.Close()
+// 	readFile("./myFile.txt")
+// }
+// func readFile(filename string) {
+// 	dataByte, err := ioutil.ReadFile(filename)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println("Text Data inside the file is: ", string(dataByte))
+// }
+
 func main() {
-	fmt.Println("Hey WelCome to Golang")
-	content := "This is the text that Goes into the file"
-	file, err := os.Create("./myFile.txt")
-	if err != nil {
-		panic(err)
-		//Panic will shout down the exection of the program
-	}
-	length, err := io.WriteString(file, content)
-	//io.WriteString will give err and length
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Length is: ", length)
-	defer file.Close()
-	readFile("./myFile.txt")
+	fmt.Println("File Handling in Golang")
+	context := "This is to be written in Golang using file Handling"
+	Createfile("./hello.txt", context)
+
 }
-func readFile(filename string) {
-	dataByte, err := ioutil.ReadFile(filename)
+func Createfile(filename string, context string) {
+	file, err := os.Create(filename)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Text Data inside the file is: ", string(dataByte))
+	_, err = io.WriteString(file, context)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 }
