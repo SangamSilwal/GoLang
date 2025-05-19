303f0651be3097c3238007bf3546f8282fd0f48f
SangamSilwal <sangamsilwal2062@gmail.com>
2025-05-19 22:49:08 +0545

    get request in golang

---
+++ 18getRequest/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Https methods in golang")
+	PerformGetRequest()
+
+}
+
+func PerformGetRequest() {
+	const myurl = "https://api.freeapi.app/api/v1/public/randomusers?page=1&limit=10"
+	response, err := http.Get(myurl)
+	if err != nil {
+		panic(err)
+	}
+	//Closing the request in Golang
+	defer response.Body.Close()
+
+	fmt.Println("Status Code : ", response.StatusCode)
+	fmt.Println("Content Length: ", response.ContentLength)
+
+	// content, _ := ioutil.ReadAll(response.Body)
+	// fmt.Println(string(content))
+
+	var responseString strings.Builder
+	content, _ := ioutil.ReadAll(response.Body)
+	byteCount, _ := responseString.Write(content)
+	fmt.Println(byteCount)
+	// fmt.Println(responseString.String()) --> this will print out all the reponse from the get url
+
+}
