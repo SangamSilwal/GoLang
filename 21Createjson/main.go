c13ef3b849434e6ca6fabb893da42baede9c2c5a
SangamSilwal <sangamsilwal2062@gmail.com>
2025-05-20 21:46:18 +0545

    Validating json in golang

--- 21Createjson/main.go
+++ 21Createjson/main.go
@@ -18,7 +18,7 @@ type course struct {
 
 func main() {
 	fmt.Println("Json Handler in Golang")
-	EncodeJson()
+	DecodeJson()
 
 }
 
@@ -47,5 +47,37 @@ func EncodeJson() {
 	fmt.Printf("%s", modifiedJson)
 	contentJson := string(modifiedJson)
 	fmt.Printf("%T", contentJson)
+}
+
+func DecodeJson() {
+	//Json data from Web is in bytes
+	jsonDataFromWeb := []byte(`
+	            {
+                "coursename": "SpingBoot BootCamp",
+                "Price": 299,
+                "Platform": "Youtube",
+                "Password": "asdad",
+                "tags": ["WebDev","Python","js"]
+				}
+	`)
+	fmt.Println(jsonDataFromWeb) //This will print the jsonDataFromWeb in byte format
+	var courseStruct course
+	checkvalid := json.Valid(jsonDataFromWeb)
+	if checkvalid {
+		fmt.Println("Json is Valid")
+		json.Unmarshal(jsonDataFromWeb, &courseStruct)
+		fmt.Printf("%#v\n", courseStruct)
+	} else {
+		fmt.Println("The json was not valid")
+	}
+
+	//some cases where you just want to add data to key value pair
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Println(myOnlineData)
+
+	for key, value := range myOnlineData {
+		fmt.Printf("key : %v and value: %v\n", key, value)
+	}
 
 }
